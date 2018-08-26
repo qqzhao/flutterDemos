4e93113b5aabfd0ab9828c7fd687fffb2d138d03
qianzhao <qianzhao@tencent.com>
2018-08-27 01:12:24 +0800

    use MyRouter to refactor.

---
+++ examples/hello/lib/config/router_config.dart
@@ -0,0 +1,62 @@
+import 'package:hello/home/animation/animation.dart';
+import 'package:hello/home/animation/animation2.dart';
+import 'package:hello/home/animation/animation3.dart';
+import 'package:hello/home/animation/animation4.dart';
+import 'package:hello/home/animation/animation5.dart';
+import 'package:hello/home/animation/animation6.dart';
+import 'package:hello/home/animation/animation7.dart';
+
+import 'package:hello/home/base/caculate_size.dart';
+import 'package:hello/home/base/fractionally_sized_box.dart';
+import 'package:hello/home/base/future_builder.dart';
+import 'package:hello/home/base/image_center_slice.dart';
+import 'package:hello/home/base/listview.dart';
+import 'package:hello/home/base/objectdbPage.dart';
+import 'package:hello/home/base/refresh_indicator.dart';
+import 'package:hello/home/base/textfield.dart';
+import 'package:hello/home/combine/bottomSheet.dart';
+import 'package:hello/home/combine/dialog.dart';
+import 'package:hello/home/combine/loading.dart';
+import 'package:hello/home/combine/pop_drag_view.dart';
+import 'package:hello/home/combine/popview.dart';
+import 'package:hello/home/combine/video_demo.dart';
+
+import 'package:hello/utils/route.dart';
+
+List<dynamic> _globalRouters = [
+  MyRouterList(name: 'Base', lists: [
+    MyRouter(name: 'TestField', routeName: '/base/textfield', widget: new TextFieldPage()),
+    MyRouter(name: 'ListView1', routeName: '/base/listview1', widget: new ListViewPage1()),
+    MyRouter(name: 'Future Build', routeName: '/base/flutter_build', widget: new FutureBuildPage()),
+    MyRouter(name: 'Refresh Indicator', routeName: '/base/refresh_indicator', widget: new RefreshIndicatorPage()),
+    MyRouter(name: 'FractionallySizedBox', routeName: '/base/fractional_sizebox', widget: new FractionallBoxPage()),
+    MyRouter(name: 'ImageCenterSlicePage', routeName: '/base/imagecenter_slice', widget: new ImageCenterSlicePage()),
+    MyRouter(name: 'ObjectdbTestPage', routeName: '/base/objectpage', widget: new ObjectdbTestPage()),
+    MyRouter(name: 'CaculatePage', routeName: '/base/caculate_page', widget: new CaculatePage()),
+  ]),
+  MyRouterList(name: 'Animation', lists: [
+    MyRouter(name: 'animation', widget: new AnimationPage()),
+    MyRouter(name: 'animation2', widget: new AnimatePage2()),
+    MyRouter(name: 'animation3', widget: new AnimatePage3()),
+    MyRouter(name: 'animation4', widget: new AnimatePage4()),
+    MyRouter(name: 'animation5', widget: new AnimatePage5()),
+    MyRouter(name: 'animation6', widget: new AnimatePage6()),
+    MyRouter(name: 'animation7', widget: new AnimatePage7()),
+  ]),
+  MyRouterList(name: 'Combine', lists: [
+    MyRouter(name: 'Dialog', widget: new DialogPage()),
+    MyRouter(name: 'PopView', widget: new PopViewPage()),
+    MyRouter(name: 'PopDragView', widget: new PopDragViewPage()),
+    MyRouter(name: 'Loading', widget: new LoadingPage()),//VideoDemo
+    MyRouter(name: 'VideoDemo', widget: new VideoDemo()),
+    MyRouter(name: 'BottomSheet', widget: new BottomSheetPage()),
+  ]),
+  MyRouterList(name: 'others', lists: [
+
+  ]),
+];
+
+var globalRouters = new MyRouterList(
+  lists: _globalRouters,
+  name: 'flutter demos'
+);
--- examples/hello/lib/main.dart
+++ examples/hello/lib/main.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hello/config/router_config.dart';
 import 'package:hello/global.dart';
-import 'package:hello/home/base/base.dart';
 import 'package:hello/home/base/objectdbPage.dart';
 import 'package:hello/utils/route.dart';
-import './home/home.dart';
 import './components/custom_navigator_observe.dart';
 
 void main() => runApp(new MyApp());
-
-List<dynamic> globalRouters = [
-  MyRouterList(name: '/base', lists: [
-    MyRouter(name: '/base/objectpage', widget: ObjectdbTestPage()),
-  ]),
-  MyRouter(name: '/base/objectpage', widget: ObjectdbTestPage()),
-  MyRouter(name: '/base/objectpage2', widget: ObjectdbTestPage()),
-
-];
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -41,60 +31,11 @@ class MyApp extends StatelessWidget {
         primaryColorBrightness: curBright,
         accentColorBrightness: curBright,
       ),
-      home: new HomePage2(),
+      home: new RouterPage(routerList: globalRouters,),
       navigatorKey: globalKey,
       routes: {
         '/base/objectpage': (BuildContext context) => new ObjectdbTestPage(),
-        '/base': (BuildContext context) => new BasePage(),
       },
-    );
-  }
-}
-
-class HomePage2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('flutter demos'),
-      ),
-      body: new Container(
-        color: Colors.white,
-        child: new ListView.builder(
-            itemCount: globalRouters.length,
-            shrinkWrap: true,
-            itemExtent: 80.0,
-            itemBuilder: (context, index) {
-              var item = globalRouters[index];
-              Widget cell = new Container(
-                decoration: new BoxDecoration(
-                  color: Colors.white,
-                  border: new Border(
-                      bottom: const BorderSide(
-                        color: Color(0x3f000000),
-                        width: 1.0,
-                      )
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new Text(item.name),
-                  ],
-                ),
-              );
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(context, new MaterialPageRoute(
-                    builder: (_){
-                      return item.route;
-                    },
-                  ));
-                },
-                child: cell,
-              );
-            }),
-      ),
     );
   }
 }
