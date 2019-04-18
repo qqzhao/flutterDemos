f5ab1768f7562cb181a39477ff5aab73489c79c7
qianzhao <qianzhao@tencent.com>
2019-04-18 19:31:37 +0800

    add cross_test/TestTwo

---
+++ examples/cross_test/TestTwo/Pods/Target Support Files/Pods-TestTwoUITests/Pods-TestTwoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestTwoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestTwoUITests
+@end
---
+++ examples/cross_test/TestTwo/TestTwo/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  TestTwo
+//
+//  Created by 赵千千 on 2019/4/18.
+//  Copyright © 2019 qqzhao. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Flutter/Flutter.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [super viewDidLoad];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button addTarget:self
+               action:@selector(handleButtonAction)
+     forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:@"Press me" forState:UIControlStateNormal];
+    [button setBackgroundColor:[UIColor blueColor]];
+    button.frame = CGRectMake(80.0, 210.0, 160.0, 40.0);
+    [self.view addSubview:button];
+}
+
+- (void)handleButtonAction {
+    FlutterViewController* flutterViewController = [[FlutterViewController alloc] init];
+    [self presentViewController:flutterViewController animated:false completion:nil];
+    //    [flutterViewController setInitialRoute:@"route1"];
+//    [self.navigationController pushViewController:flutterViewController animated:YES];
+}
+
+@end
